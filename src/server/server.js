8d39fae9aa18910c013a49e7e541897fd181663b
ik264 <264@tranthinktech.com>
2023-04-17 15:19:29 +0800

    update

--- src/server/server.js
+++ src/server/server.js
@@ -6,16 +6,42 @@ const port = 6969;
 const server = http.createServer(express);
 const wss = new WebSocket.Server({ server })
 
+const CLIENTS = {};
+var connectionIDCounter = 0;
 wss.on('connection', function connection(ws) {
-    ws.on('message', function incoming(data) {
-        wss.clients.forEach(function each(client) {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(data);
-            }
+    ws.id = connectionIDCounter++;
+    CLIENTS[ws.id] = ws;
+    console.log(CLIENTS);
+    // if (ws.id == 2) {
+    //     ws.on('message', function (msg) {
+    //         data = JSON.parse(msg);
+    //         CLIENTS['2'].send("Hello client 2");
+    //     })
+    // }
+    if (ws.id == 0) {
+        ws.on('message', function incoming(data) {
+            CLIENTS['2'].send(data);
         })
-    })
+    }
+    if (ws.id == 2) {
+        ws.on('message', function incoming(data) {
+            CLIENTS['0'].send(data);
+        })
+    }
+    if (ws.id == 1) {
+        ws.on('message', function incoming(data) {
+            CLIENTS['2'].send("Hello World");
+        })
+    }
+    // ws.on('message', function incoming(data) {
+    //     wss.clients.forEach(function each(client) {
+    //         if (client !== ws && client.readyState === WebSocket.OPEN) {
+    //             client.send(data);
+    //         }
+    //     })
+    // })
 })
 
-server.listen(port, function() {
+server.listen(port, function () {
     console.log(`Server is listening on ${port}!`)
 })
